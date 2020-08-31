636b5412f4024189a621458e14b2cdd82c497675
Wai Min <leopard23@gmail.com>
2020-08-31 21:32:37 +0800

    Add search placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className='flex content-center flex-wrap'>
+    <div className='flex content-center flex-wrap bg-yellow-400'>
       <h1 className='m-5 w-full text-center text-bold text-5xl'>
         Square Countries
       </h1>
+      <Search />
     </div>
   );
 }
