e664298524b6d9fb30685fa6ccb47f9867dbbeba
Wai Min <leopard23@gmail.com>
2020-08-31 21:26:16 +0800

    Add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function App() {
-  return <h1>Square Countries</h1>;
+  return (
+    <div className='flex content-center flex-wrap'>
+      <h1 className='m-5 w-full text-center text-bold text-5xl'>
+        Square Countries
+      </h1>
+    </div>
+  );
 }
 
 export default App;
