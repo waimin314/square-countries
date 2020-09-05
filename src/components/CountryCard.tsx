a3c47ee4199e074c33d2f81b8237ce6aa70eb32f
Wai Min <leopard23@gmail.com>
2020-09-05 11:54:57 +0800

    Use larger radius for rounded corner

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -12,7 +12,7 @@ export const CountryCard: React.FC<countryProps> = ({
   name,
 }) => {
   return (
-    <div className='bg-white shadow-lg w-40 h-64 rounded-lg p-5'>
+    <div className='bg-white shadow-lg w-40 h-64 rounded-xl p-5'>
       <div className='bg-teal-400 w-full h-20 mb-5'></div>
       <div className='w-full h-20 border border-gray-500'>
         <img src={flagURL} alt={`flag of ${name}`}></img>
